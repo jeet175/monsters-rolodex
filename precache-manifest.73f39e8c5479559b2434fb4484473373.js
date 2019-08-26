8b17de91d3f5c6b9f3811d09f333175c3e588fd4
jeet175 <47863285+jeet175@users.noreply.github.com>
2019-08-26 14:54:20 +0530

    Updates

---
+++ precache-manifest.73f39e8c5479559b2434fb4484473373.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "64f59c09f8b5548450f9ba11b27dd3bb",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "ac37df398f2837697727",
+    "url": "/monsters-rolodex/static/css/main.649c4379.chunk.css"
+  },
+  {
+    "revision": "86e57c060d9fa6a86926",
+    "url": "/monsters-rolodex/static/js/2.0e7fe84b.chunk.js"
+  },
+  {
+    "revision": "ac37df398f2837697727",
+    "url": "/monsters-rolodex/static/js/main.49ab711e.chunk.js"
+  },
+  {
+    "revision": "7548f6fbafc46e9f04b0",
+    "url": "/monsters-rolodex/static/js/runtime~main.4eb04847.js"
+  }
+]);
